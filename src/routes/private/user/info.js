c7333d8e885df808c7a1feab0d50e44ef763e32a
bajinho <5te6efhu4@mozmail.com>
2024-12-27 19:42:23 -0300

    info do usuario

--- src/routes/private/user/info.js
+++ src/routes/private/user/info.js
@@ -1,40 +1,39 @@
 import crypto from "crypto";
 import moment from "moment";
-import { User } from "../../../db/model";
 
 export default async (req, res) => {
   try {
     const { user, token } = req;
 
-    const Exp = new Date(token.exp * 1000);
+    if (!user || !token) {
+      return res.status(400).send({
+        code: 0,
+        message: "Usuário ou token não fornecido.",
+      });
+    }
 
-    const Indicados = await User.findAll({
-      where: {
-        usuario_id_patrocinador: user.usuario_id,
-      },
-    });
+    if (!token.exp) {
+      return res.status(400).send({
+        code: 0,
+        message: "Token não contém data de expiração.",
+      });
+    }
+
+    const Exp = new Date(token.exp * 1000);
 
     return res.send({
       code: 1,
       message: true,
       userInfo: {
-        _id: crypto.createHash("md5").update(user.usuario_id).digest("hex"),
-        uLogin: user.usuario_login,
+        _id: crypto.createHash("md5").update(user.usuario_id.toString()).digest("hex"),
         uNome: user.usuario_nome,
-        uAvatar: user.usuario_avatar,
-        uGraduacao: user.usuario_graduacao,
-        uLinkIndicacion: `site.com/${user.usuario_login}`, //FIXME:: Colocar o link certo
 
-        uSessionExpirationNow: moment().format("dd/mm/yyyy hh:mm:ss"),
-        uSessionExpiration: moment(Exp).format("dd/mm/yyyy hh:mm:ss"),
+        uSessionExpirationNow: moment().format("DD/MM/YYYY HH:mm:ss"),
+        uSessionExpiration: moment(Exp).format("DD/MM/YYYY HH:mm:ss"),
 
         uEmail: {
-          verificado: false, //FIXME: Tem muito valor "hard-coded", você não deveria retirar eles de algum lugar?
-          email: user.usuario_email,
-        },
-        uTelefone: {
           verificado: false,
-          telefone: user.usuario_telefone,
+          email: user.usuario_email,
         },
         uSeguranca: {
           auth: false,
@@ -43,23 +42,10 @@ export default async (req, res) => {
       },
     });
   } catch (err) {
-    res.send({
+    console.error(err); // Adiciona o log do erro para ajudar na depuração
+    res.status(500).send({
       code: 0,
       message: "Alguma coisa deu errado. Entre em contato com a administração.",
     });
   }
 };
-
-/*
-// It was already commented out in the original source
-  'uInvestimentos' => array(
-      'total' => $this->db->where('cota_id_dono', $userID)->from('tb_cotas')->count_all_results(),
-      'pendente' => "0", //$this->db->where('cota_id_dono', $userID)->where('cota_status', 0)->from('tb_cotas')->count_all_results(),
-      'pendente_valor' => $this->Utilidades->valor(0), //$this->Utilidades->valor($this->db->select_sum('cota_valor')->where('cota_id_dono', $userID)->where('cota_status', 0)->from('tb_cotas')->get()->result_array()[0]['cota_valor']),
-      'pendente_hash' => $this->db->select('cota_id_md5')->where('cota_id_dono', $userID)->where('cota_status', 0)->from('tb_cotas')->get()->result_array(),
-      'ativos' => $this->db->where('cota_id_dono', $userID)->where('cota_status', 1)->from('tb_cotas')->count_all_results(),
-      'ativos_valor' => $this->Utilidades->valor(0), //$this->Utilidades->valor($this->db->select_sum('cota_valor')->where('cota_id_dono', $userID)->where('cota_status', 1)->from('tb_cotas')->get()->result_array()[0]['cota_valor']),
-      'finalizados' => "0", //$this->db->where('cota_id_dono', $userID)->where('cota_status', 2)->from('tb_cotas')->count_all_results(),
-      'finalizados_valor' => $this->Utilidades->valor(0), //$this->Utilidades->valor($this->db->select_sum('cota_valor')->where('cota_id_dono', $userID)->where('cota_status', 2)->from('tb_cotas')->get()->result_array()[0]['cota_valor']),
-  )
-*/
